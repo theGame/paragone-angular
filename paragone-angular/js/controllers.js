e91797e8ecbae3b213a2102a08721bf5987e9424
theGame <03111987a>
2015-01-05 21:09:30 +0200

    have been added common filter :) and value service but value doesn't work

--- paragone-angular/js/controllers.js
+++ paragone-angular/js/controllers.js
@@ -1,8 +1,10 @@
-app.controller('bodyCtrl', function(){
-	// test string
-});
+app.value('startPoint', '0');
 
-app.controller('CarouselDemoCtrl', function($scope, $http, $animate){
+app.controller('bodyCtrl', [ function(){
+	// test string
+}]);
+
+app.controller('CarouselDemoCtrl', [ function($scope, $http, $animate){
 	$http.get('jsonfile/investors.json').success(function(data, config, headers, status){
 		$scope.investorData = data; 
 		console.log('---------------------mainSlider------------------------success');
@@ -11,4 +13,15 @@ app.controller('CarouselDemoCtrl', function($scope, $http, $animate){
 	.error(function(data, status, headers, config){
 		$scope.investorData = status;
 	});
-});
+}]);
+
+app.filter('cutText', ['startPoint', function(startPoint){
+	return function(post, startPoint){
+		var res; 
+		var emptyIndex;
+		res = post.slice(0, 60);
+		emptyIndex = res.lastIndexOf(' ');
+		res = res.slice(0, emptyIndex);
+		return res = res + ' ...';
+	}
+}]);
