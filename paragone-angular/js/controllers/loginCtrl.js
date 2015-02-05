40c6f3c532f75360c4d3ae7c035fe8dc4511f411
theGame <voytas1991@gmail.com>
2015-02-05 23:51:43 +0200

    added some functionality =)

--- paragone-angular/js/controllers/loginCtrl.js
+++ paragone-angular/js/controllers/loginCtrl.js
@@ -1,5 +1,16 @@
 'use strict';
 
-login.controller('loginCtrl', function($scope){
-	$scope.test = "testasdasd";
+login.controller('loginCtrl', function($scope, $sessionStorage){
+	
+	var existingUser ={
+		"name" : "yuriy.voytas",
+		"password" : "test"  
+	}
+
+	$scope.checkUser = function(username, pass){
+		if(existingUser.name == this.username && existingUser.password == this.pass){
+			alert("yes");
+		}
+	};
+
 });
